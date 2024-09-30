538a52e082d1ec07d33171375c3735c34c2f15ca
dev-kumaralingam <kumaralingam2384@gmail.com>
2024-09-30 20:49:25 +0530

    Fourteenth commit

---
+++ Pass.py
@@ -0,0 +1,12 @@
+def Continue(n):
+    i=1
+    while(i):
+        if(i==5):
+            pass
+        if(i==n):
+            break
+        print(i)
+        i+=1
+
+n=int(input("Enter a value: "))
+Continue(n)
