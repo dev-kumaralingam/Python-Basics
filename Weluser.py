adbb2bb3d2d11069677c6b60eda192e72ac235a9
dev-kumaralingam <kumaralingam2384@gmail.com>
2024-12-05 19:44:40 +0530

    Seventyfifth commit

--- Weluser.py
+++ Weluser.py
@@ -1,6 +1,17 @@
+import datetime
+def greetuser():
+    current_time=datetime.time
+    hour=current_time.hour
+    # if hour<12:
+    #     print("Good morning")
+    # elif hour<18:
+    #     print("Good afternoon")
+    # else:
+    #     print("Good evening")
 def welcome(f,l):
     print(f"Welcome to the game, {f}{l}!")
 
 f=input("Enter a first name: ")
 l=input("Enter a last name: ")
+greetuser()
 welcome(f,l)  
