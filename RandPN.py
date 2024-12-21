0d128e5036fe3cd82ac5596e4b5957b9f10d87a6
dev-kumaralingam <kumaralingam2384@gmail.com>
2024-12-21 17:34:45 +0530

    Ninetyone commit

---
+++ RandPN.py
@@ -0,0 +1,17 @@
+import random
+
+# Get input range from the user
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+
+# Generate a random number within the range
+random_number = random.randint(start, end)
+
+# Print the random number
+print(f"The random number is: {random_number}")
+
+# Check if the random number is Positive or Negative
+# if random_number >= 0:
+#     print(f"{random_number} is Positive.")
+# else:
+#     print(f"{random_number} is Negative.")
