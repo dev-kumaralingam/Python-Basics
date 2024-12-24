90b3b4c2b189805e1832a980e9fe6c03cefd814d
dev-kumaralingam <kumaralingam2384@gmail.com>
2024-12-24 19:00:58 +0530

    Ninetyfourth commit

--- RandPN.py
+++ RandPN.py
@@ -8,10 +8,10 @@ end = int(input("Enter the end of the range: "))
 random_number = random.randint(start, end)
 
 # Print the random number
-print(f"The random number is: {random_number}")
+print(f"The random number is : {random_number}")
 
 # Check if the random number is Positive or Negative
-if random_number >= 0:
-    print(f"{random_number} is Positive.")
-else:
-    print(f"{random_number} is Negative.")
+# if random_number >= 0:
+#     print(f"{random_number} is Positive.")
+# else:
+#     print(f"{random_number} is Negative.")
